77e3d748c4f66f10ab65e7d51b4734615a7ad1f3
Utku Koçlar <64081851+utkukoclar@users.noreply.github.com>
2024-10-23 22:09:22 +0300

    Add files via upload

---
+++ 04_Loops/Program.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region For Döngüsü
+            //For(x;y;z)
+            //x: başlangıç değeri
+            //y: bitiş
+            //z: artış-azalış
+
+            //int i;
+            //for(i=1;i<=5;i++)
+            //{
+            //    Console.WriteLine("C# Eğitim Kampı");
+            //}
+
+            //for (int i = 1;i<=20;i++)
+            //{
+            //    Console.WriteLine(i); ;
+            //}
+
+            //for(int i=3;i<=50;i+=3)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //Console.Write("Lütfen ekrana yazılmasını istediğiniz adedi giriniz: ");
+            //int finishValue = int.Parse(Console.ReadLine());
+
+            //for (int i = 1; i <= finishValue; i++)
+            //{
+            //    Console.WriteLine("Yaşasın Cumhuriyet");
+            //}
+
+
+            #endregion
+
+            #region For Döngüsü ile Karar Yapıları
+            //for(int i=1;i<=100;i++)
+            //{
+            //    if(i%5==0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }    
+            //}
+
+            //ÖRN: 1-10 arasındaki sayıları toplama
+            //int totalValue = 0;
+            //for (int i = 1;i<=10;i++)
+            //{
+            //    totalValue += i;
+
+            //}
+            //Console.WriteLine(totalValue);
+
+            //ÖRN: 1-20 arasındaki çift sayıların toplamı
+            //int totalValue = 0;
+            //for (int i = 1; i < 20; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        totalValue += i;
+            //        Console.WriteLine(i);
+
+            //    }
+            //}
+            //Console.WriteLine("--------------");
+            //Console.WriteLine(totalValue);
+
+            // ÖRN: 1-50 arasında 7'ye tam bölünen kaç sayı olduğunu bulan program.
+            //int count = 0;
+            //for (int i = 1; i <= 50; i++)
+            //{
+            //    if (i % 7 == 0)
+            //    {
+            //        count++;
+            //    }
+
+            //}
+            //Console.WriteLine(count);
+
+            //ÖRN: Bir bakteri türü her saatin sonunda kendini ikiye bölerek çoğaltmaktadır. Yeni oluşan bakteriler 2'ye bölünerek çoğalıyor. 24 saatin sonunda kaç bakteri olur?
+            //int bacterium = 1;
+            //for (int i = 1;i<=24;i++)
+            //{
+            //    bacterium *= 2;
+            //    Console.WriteLine(i + ".Saat sonunda"+bacterium);
+            //}
+            #endregion
+
+            #region While Döngüsü
+            //while: şart sağlandığı müddetçe
+            //while(Şart)
+            //{
+            //    (işlemler)
+            //}
+
+            // ÖRN: Ekrana 10 defa merhaba döngüler yazan program.
+            //int i = 1;
+            //while (i <= 10)
+            //{
+            //    Console.WriteLine("Merhaba döngüler");
+            //    i++;
+            //}
+
+            //ÖRN: 1-10 arasında 3'e tam bölünen sayıları bulan program.
+            //int i = 1;
+            //while (i<=10)
+            //{
+            //    if (i%3==0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //    i++;
+            //}
+
+            //ÖRN: 1 ile 10 arasındaki sayıların toplamını hesaplayan program.
+            //int i = 1;
+            //int sum = 0;
+            //while(i<=10)
+            //{
+            //    sum += i;
+            //    i++;
+            //}
+            //Console.WriteLine(sum);
+
+
+            #endregion
+
+            #region Örnek Sınav Sorusu
+            //ÖRN: Klayyeden girilen 3 basamaklı sayının basamakları toplamını hesaplayan kodu yazınız.
+            //456
+
+            //Console.Write("Sayıyı giriniz: ");
+            //int number = int.Parse(Console.ReadLine());
+
+            //int ones, tens, hundreds;
+            //int sum;
+
+            //ones = number % 10; //sayının 10'a bölümünden kalan 1ler basamağını verir.
+            //tens = (number % 100) / 10;
+            //hundreds = number / 100;//sayıyı 100'e bölümünden kalan yüzler basamağını verir.
+
+
+            //Console.WriteLine(ones + "-" + tens + "-" + hundreds);
+
+            //sum = ones + tens + hundreds;
+
+            //Console.WriteLine(sum);
+            #endregion
+            Console.Read();
+        }
+    }
+}
---
+++ 08_Methods/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Void Methodlar
+
+            //()
+            //Geriye değer döndürmeyen methodlar
+            //Customer-Listele,ekle,sil,güncelle
+            //void
+
+            //void CustomerList()
+            //{
+            //    Console.WriteLine("Ali Yıldız");
+            //    Console.WriteLine("Ayşe Yıldız");
+            //    Console.WriteLine("Hakan Öztürk");
+            //    Console.WriteLine("Merve Çınar");
+            //}
+
+            //CustomerList();
+            //CustomerList();
+            //CustomerList();
+            //CustomerList();
+
+            //void Sum() 
+            //{
+            //    int x = 1;
+            //    int y = 2;
+            //    int z = x + y;
+            //    Console.WriteLine(z);
+            //}
+            //Sum();
+            #endregion
+
+            #region Geriye değer döndürmeyen string parametreli methodlar
+            //void WriteMethod(string customerName)
+            //{
+            //    Console.WriteLine(customerName);
+            //}
+            //WriteMethod("Mehmet Yıldırım");
+
+            //void CustomerCard(string name, string Surname)
+            //{
+            //    Console.WriteLine("Müşteri:" + name + " " + Surname);
+            //}
+            //CustomerCard("Mehmet", "Yıldız");
+            //CustomerCard("Ayşegül","Kaya");
+            #endregion
+
+            #region Geriye değer döndürmeyen int parametreli methodlar
+            //Dışarıdan alınan 3 tane sayıyı toplayan method
+            //void Sum(int number1, int number2, int number3)
+            //{
+            //    int result = number1 + number2 + number3;
+            //    Console.WriteLine(result);
+            //}
+            //Sum(4, 5, 6);
+            #endregion
+
+            #region Geriye Değer Döndüren Methodlar
+            //string CustomerName()
+            //{
+            //    return "Buse Yıldız";
+            //}
+            //CustomerName();
+
+            //string StudentCard()
+            //{
+            //    string name = "Ali";
+            //    string surname = "Kaya";
+
+            //    return name + " " + surname;
+            //}
+            //Console.WriteLine(StudentCard());
+            #endregion
+
+            #region Geriye değer döndüren string parametreli methodlar
+            //string CountryCard(string countryName, string capital, string flagColor)
+            //{
+            //    string cardInfo="Ülke: "+ countryName + "-  Başkent: " + capital + "- Bayrak Rengi:" +flagColor;
+            //    return cardInfo;
+            //}
+            //string x, y, z;
+            //Console.Write("Ülke adını giriniz: ");
+            //x=Console.ReadLine();
+
+            //Console.Write("Başkenti giriniz: ");
+            //y=Console.ReadLine();
+
+            //Console.Write("Bayrak rengi giriniz: ");
+            //z=Console.ReadLine();
+
+            //Console.WriteLine(CountryCard(x, y, z));
+
+            //Console.WriteLine(CountryCard("Türkiye", "Ankara", "Kırmızı-Beyaz"));
+            #endregion
+
+            #region Geriye değer döndüren int parametreli methodlar
+            //int Sum(int number1,int number2)
+            //{
+            //    int result = number1 + number2;
+            //    return result;
+            //}
+            //Console.WriteLine(Sum(45, 98));
+            //Console.WriteLine(Sum(36, 25));
+            //Console.WriteLine(Sum(44, 36));
+            //Console.WriteLine(Sum(14, 20));
+            #endregion
+
+            #region Örnek Uygulama
+            
+            string ExamResult(string Student, int exam1,int exam2, int exam3)
+            {
+                int result = (exam1 + exam2 + exam3) / 3;
+                if (result >= 50)
+                {
+                    return Student + " isimli Öğrenci Sınavı geçti " + "Ortalama "+result;
+                }
+                else
+                {
+                    return Student + " isimli Öğrenci Sınavı geçemedi " + "Ortalama " + result;
+
+
+                }
+            }
+            Console.WriteLine(ExamResult("Ali", 25, 41, 85));
+            Console.WriteLine(ExamResult("Ayşe", 36, 88, 33));
+            #endregion
+
+            Console.Read();
+        }
+    }
+}
